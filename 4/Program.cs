7c0b0a581bc5c5d6e31f037fec6cab6204e84067
Anna Golikova <annagolikovast@gmail.com>
2022-08-24 14:27:13 +0400

    4 finish

---
+++ 4/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Enter first number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter third number: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (numberB > max && numberB > numberC)
+{
+    max = numberB;
+    Console.WriteLine("max number = " + max);
+}
+
+else 
+    if (numberC > max && numberC > max)
+    {
+        max = numberC;
+        Console.WriteLine("max number = " + max);
+    }
+
+    else 
+    {
+        max = numberA;
+        Console.WriteLine("max number = " + max);    
+    }
