b59c7a36ccc7dd11b9aa42f5f61f99dca3c91d01
Anna Golikova <annagolikovast@gmail.com>
2022-08-24 15:19:00 +0400

    add new condition equal numbers

--- 2/Program.cs
+++ 2/Program.cs
@@ -6,14 +6,21 @@ int numberB = Convert.ToInt32(Console.ReadLine());
 
 int max = numberA;
 
-if (numberB > max)
+if (numberB == max)
 {
-    max = numberB;
-    Console.WriteLine("max number = " + max);
+    Console.WriteLine("numbers are equal");
 }
 
 else 
-{
-     max = numberA;
-    Console.WriteLine("max number = " + max);
-}
+
+    if (numberB > max)
+    {
+        max = numberB;
+        Console.WriteLine("max number = " + max);
+    }
+
+    else 
+    {
+        max = numberA;
+        Console.WriteLine("max number = " + max);
+    }
