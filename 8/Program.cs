70644fc40ebc66cf43096088e8b5326daf0bc73a
Anna Golikova <annagolikovast@gmail.com>
2022-08-24 15:08:34 +0400

    8 finish

---
+++ 8/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Enter positive number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+if(number <= 0)
+{
+    Console.WriteLine("Enter the number greater than zero!");
+}
+
+else
+{
+
+    while(i <= number) 
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write(i +" ");
+        }
+
+    i++;
+    }
+}
